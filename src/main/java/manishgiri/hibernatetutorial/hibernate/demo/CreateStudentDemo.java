fe2188648640a934ab29be396ec5f4b30e3d2724
MANISH-GIRI <manish.giri.me@gmail.com>
2019-07-15 17:18:01 -0400

    Create and save Student object in MySQL w/ Hibernate

---
+++ src/main/java/manishgiri/hibernatetutorial/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,41 @@
+package manishgiri.hibernatetutorial.hibernate.demo;
+
+import manishgiri.hibernatetutorial.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateStudentDemo {
+
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            Student newStudent = new Student("John", "Doe", "john.doe@gmail.com");
+
+            session.beginTransaction();
+
+            System.out.println("Saving the student...");
+            session.save(newStudent);
+
+            // commit
+            session.getTransaction().commit();
+
+            System.out.println("Done...");
+
+        }
+
+        catch (Exception e) {
+            System.out.println("Failed: " + e);
+        }
+
+        finally {
+            session.close();
+        }
+
+
+    }
+}
