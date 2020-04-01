ed7e8851df5da0fb64b2580b7d8707832ec72479
MANISH-GIRI <manish.giri.me@gmail.com>
2020-03-31 23:53:06 -0400

    Generation Strategy(Identity) is mandatory for updating ID properly

--- src/main/java/manishgiri/hibernatetutorial/hibernate/demo/entity/Student.java
+++ src/main/java/manishgiri/hibernatetutorial/hibernate/demo/entity/Student.java
@@ -1,15 +1,13 @@
 package manishgiri.hibernatetutorial.hibernate.demo.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="student")
 public class Student {
 
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
     @Column(name = "first_name")
